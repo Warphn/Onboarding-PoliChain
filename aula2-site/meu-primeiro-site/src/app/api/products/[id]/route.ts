9b2785ced23f4f07cd00c74d5b8f1c922459ee75
Warphn <victor.bertelli@usp.br>
2025-06-16 21:02:07 -0300

    Falha id corrigido

--- aula2-site/meu-primeiro-site/src/app/api/products/[id]/route.ts
+++ aula2-site/meu-primeiro-site/src/app/api/products/[id]/route.ts
@@ -1,16 +1,25 @@
 import { prisma } from '@/app/lib/prisma';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
-  const body = await req.json();
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+
   const product = await prisma.product.update({
     where: { id: Number(params.id) },
     data: body,
   });
+
   return NextResponse.json(product);
 }
 
-export async function DELETE(_req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(
+  _request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   await prisma.product.delete({ where: { id: Number(params.id) } });
   return NextResponse.json({ ok: true });
 }
