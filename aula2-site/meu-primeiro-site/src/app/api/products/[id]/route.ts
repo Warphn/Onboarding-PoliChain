18c65c238298e04c5bb463531daa0b160af8b154
Warphn <victor.bertelli@usp.br>
2025-06-16 21:20:18 -0300

    Nova tentativa

--- aula2-site/meu-primeiro-site/src/app/api/products/[id]/route.ts
+++ aula2-site/meu-primeiro-site/src/app/api/products/[id]/route.ts
@@ -1,27 +1,26 @@
-import { prisma } from '@/app/lib/prisma';
-import { NextResponse } from 'next/server';
-
+import { prisma } from "@/app/lib/prisma";
+import { NextResponse } from "next/server";
 
 export async function PUT(
   request: Request,
-  { params }: { params: { id: string } }   
+  { params }: { params: Promise<{ id: string }> }   
 ) {
+  const { id } = await params;                      
   const body = await request.json();
-  const id = Number(params.id);
 
   const product = await prisma.product.update({
-    where: { id },
+    where: { id: Number(id) },
     data: body,
   });
 
   return NextResponse.json(product);
 }
 
-
 export async function DELETE(
   _request: Request,
-  { params }: { params: { id: string } }   
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  await prisma.product.delete({ where: { id: Number(params.id) } });
+  const { id } = await params;
+  await prisma.product.delete({ where: { id: Number(id) } });
   return NextResponse.json({ ok: true });
 }
